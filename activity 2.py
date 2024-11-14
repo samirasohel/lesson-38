747da594faa97c4450a2c4fffef37fe140e88a82
samirasohel <samirasohel.othoi@gmail.com>
2024-11-14 19:50:44 +0600

    ac

---
+++ activity 2.py
@@ -0,0 +1,18 @@
+#Store two numbers of your choice in two different variables. 
+# Now perform all the Arithmetic operations on those two numbers and print the results.
+
+num1=40
+num2=20
+
+print("sum of",num1,"and",num2,"is",num1+num2)
+print("Defference between",num1,"and",num2,"is",num1-num2)
+print("Product",num1,"and",num2,"is",num1*num2)
+print("Quotient",num1,"and",num2,"is",num1/num2)
+print("Floor division quotient",num1,"and",num2,"is",num1//num2)
+print("modules",num1,"and",num2,"is",num1%num2)
+print("2 to power of 4 is",2**4)
+
+#condition operator
+print("is",num1,"equal to",num2,num1==num2)
+print("is",num1,"greater than",num2,num1>num2)
+print("is",num1,"smaller than",num2,num1<num2)
